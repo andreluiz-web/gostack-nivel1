25da6e9fb4524805c03c35cffb46582984d0b195
andreluiz-web <andreluizdefreitas0@gmail.com>
2020-10-16 19:36:59 -0300

    save files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import api from './services/api';
 
-const App = () => {
+    function App() {
+
+    const [ projects, setProjects ] = useState([]);
+
+    useEffect(() => {
+        api.get('/projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+
+        const response = await api.post('/projects', {
+            title: `novo projeto ${Date.now()}`,
+            owner: 'andre luiz'
+        })
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
+    }
+
     return (
-        <h1>Hello World</h1>
+        <>
+         <Header />
+         <ul>
+            {projects.map(item => <li>{item.title}</li>)}
+         </ul>
+         <button onClick={handleAddProject}>Adicionar Projeto</button>
+        </>
     )
 }
-
+ 
 export default App;
